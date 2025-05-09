e70b5a884bede97e4ae0d0d8a8f4e2e563e31a11
sayyidazizii <sayyidsyafiq234@gmail.com>
2025-05-09 22:11:21 +0700

    Watashi Push to Git Again ...

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'sayyid',
+        //     'email' => 'sayyid@gmail.com',
+        // ]);
+
+        User::create([
+            'name' => 'sayyid',
+            'email' => 'sayyid@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+        User::create([
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
